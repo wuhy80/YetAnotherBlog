21df65a88e890ab2a313a395aedfb09ca03c52a5
James Wu <wuhy80@qq.com>
2013-05-27 22:57:23 +0800

    首页版面优化，拆分多个子Partial，并加入部分定制CSS

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery
+	protect_from_forgery
+
+	def store_location
+		session[:return_to] = request.request_uri
+	end
+
+	def redirect_back_or_default(default)
+		redirect_to(session[:return_to] || default)
+		session[:return_to] = nil
+	end
 end
